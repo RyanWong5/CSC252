5a96511361b3828f71f5d748ef6bbe79f614c376
Alex <amai2@u.rochester.edu>
2017-02-26 13:08:12 -0500

    Complete MinMaxMedian

--- MinMaxMedian.asm
+++ MinMaxMedian.asm
@@ -8,7 +8,7 @@ median: .word 0
 	.text
 	
 	# sort array
-	la $s2, nums #loads address of nums array
+	la $s2, nums #loads base address of nums array
 	la $t1, length #t1 holds address of length
 	lw $t1, 0($t1) # n load array size into t1
 	sub $s3, $s3, $s3 #initialize counter s3, a.k.a i
@@ -23,10 +23,8 @@ L2:		sub $t6, $s4, $t5 #t6 = j < (n-i) or j-(n-j)
 		lw $t7, 0($s5) #grab nums[i]
 		lw $t8, 4($s5) #grab nums[i+1]
 		sub $t9, $t8, $t7 # t9 = nums[i+1]-nums[i].
-		add $a0, $s3, $zero #argument for increment
-		add $a1, $s4, $zero #second argument for increment
 		bgtz $t9, increment #if t9 is greater than 0, then jump to increment, otherwise swap
-		add $s1, $t8, $zero #store nums[i+1] in as a register
+		add $s1, $t8, $zero #save nums[i+1] in as a register
 		sub $t8, $t8, $t8 #set nums[i+1] to 0
 		add $t8, $t7, $zero #make t8 nums[i]
 		sub $t7, $t7, $t7 #set nums[i] to 0
@@ -44,10 +42,25 @@ L3:	addi $s3, $s3, 1 #increment i by 1
 increment:	addi $s5, $s5, 4
 		addi $s4, $s4, 1
 		J L2
-		li $v0, 4
-		syscall
-	
-L4: 	li $v0, 10
-	syscall
+		
+
+#store min, max, median
+L4:	#min
+	la $t0, min
+	lw $t2, 0($s2)
+	sw $t2, 0($t0)
+
+	#max
+	la $t0, max
+	lw $t2, 40($s2)
+	sw $t2, 0($t0)
+
+	#median
+	la $t0, median
+	lw $t2, 20($s2)
+	sw $t2, 0($t0)
+
+li $v0, 10
+syscall
 	
 	
