d38a894ae6d6ad3b4bb9c2e73979eebd078130e2
Alex <amai2@u.rochester.edu>
2017-02-28 14:25:44 -0500

    Add initial data

---
+++ MergeSort.asm
@@ -0,0 +1,11 @@
+# merge sort
+#java implementation for reference - https://www.cs.cmu.edu/~adamchik/15-121/lectures/Sorting%20Algorithms/code/MergeSort.java
+.data
+nums: .word 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+sorted: .word 0:10
+length: .word 10
+.text
+
+
+
+
